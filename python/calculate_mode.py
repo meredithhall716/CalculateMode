b04a8e8bb21dd729494e6a9b3f9beb2e20a278d2
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-28 16:39:55 -0400

    first

--- python/calculate_mode.py
+++ python/calculate_mode.py
@@ -1,2 +1,24 @@
-def calculate_mode():
-    pass
+def calculate_mode(list):
+    
+    letters = {}
+    answer = []
+
+    for x in list:
+        if x in letters:
+            letters[x] += 1
+        else:
+            letters.update({x:1})   
+
+    letters = sorted(letters.items(), key = lambda x:x[1], reverse = True)
+    
+    max = letters[0][1]
+    
+    for k in letters:
+        if k[1] == max:
+            answer.append(k[0])
+
+
+
+    return answer
+
+        
